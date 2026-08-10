50e8b2142f8fc75bd9e4f26473656b1bd03f5945
rohit661199 <rohitsankhala619@gmail.com>
2026-08-10 06:31:57 +0530

    Add application constants

---
+++ constants/Option.ts
@@ -0,0 +1,33 @@
+export const TypeList = [
+    {
+        name:"Tablet",
+        icon:"https://www.flaticon.com/free-icon/pill_2800581"
+    },
+    {
+        name:"Capsule",
+        icon:"https://cdn-icons-png.flaticon.com/128/5419/5419454.png"
+    },
+    {
+        name:"Drops",
+        icon:"https://cdn-icons-png.flaticon.com/128/5641/5641781.png"
+    },
+    {
+        name:"Syrup",
+        icon:"https://cdn-icons-png.flaticon.com/128/11496/11496783.png"
+    },
+    {
+        name:"Injections",
+        icon:"https://cdn-icons-png.flaticon.com/128/3182/3182577.png"
+    }
+]
+
+export const WhenToTake = [
+    "Morning",
+    "Before Lunch",
+    "After Lunch",
+    "Afternoon",
+    "Evening",
+    "Before Dinner",
+    "After Dinner",
+    "Before Sleeping"
+]
